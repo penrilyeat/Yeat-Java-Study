497d7f4264d8da99ecfb1dc11a66f6b1ab21dcd9
penrilyeat <87742834+penrilyeat@users.noreply.github.com>
2021-07-22 11:32:46 +0800

    Add files via upload

---
+++ Java Exception Handling Tutorial/src/FinallyBlock2.java
@@ -0,0 +1,15 @@
+
+public class FinallyBlock2 {
+	public static void main(String args[]) {
+		System.out.println(FinallyBlock2.myMethod());
+	}
+
+	public static int myMethod() {
+		try {
+			return 112;
+		} finally {
+			System.out.println("This is Finally block");
+			System.out.println("Finally block ran even after return statement");
+		}
+	}
+}
